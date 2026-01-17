678aaedf67070d45ef3d4230a5686acf443ff47f
abdallasayed <37516961+abdallasayed@users.noreply.github.com>
2026-01-17 22:36:58 +0200

    Create firebase_config.dart

---
+++ lib/core/config/firebase_config.dart
@@ -0,0 +1,22 @@
+class FirebaseConfig {
+  // ⚠️ هذا للتطوير فقط - لا تستخدمه للإنتاج
+  static const Map<String, dynamic> devConfig = {
+    'apiKey': "AIzaSyABiN16MklWtX00PC6UHLSDKJCrPd9EwZs",
+    'authDomain': "loverchat190.firebaseapp.com",
+    'projectId': "loverchat190",
+    'storageBucket': "loverchat190.firebasestorage.app",
+    'messagingSenderId': "971632978916",
+    'appId': "1:971632978916:web:355f73309996d4a6d935f1",
+  };
+
+  static FirebaseOptions get options {
+    return FirebaseOptions(
+      apiKey: devConfig['apiKey']!,
+      appId: devConfig['appId']!,
+      messagingSenderId: devConfig['messagingSenderId']!,
+      projectId: devConfig['projectId']!,
+      storageBucket: devConfig['storageBucket']!,
+      authDomain: devConfig['authDomain']!,
+    );
+  }
+}
